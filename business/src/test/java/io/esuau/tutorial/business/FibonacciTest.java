68c90e71ef5ec60af7e67ab43c2d275c6e30515f
esuau <evan.suau@etu.u-pec.fr>
2017-12-13 18:55:29 +0100

    TP-11 Add second unit test

--- business/src/test/java/io/esuau/tutorial/business/FibonacciTest.java
+++ business/src/test/java/io/esuau/tutorial/business/FibonacciTest.java
@@ -14,4 +14,13 @@ public class FibonacciTest {
         Assert.assertEquals(5, Fibonacci.fibonacci(5));
     }
 
+    @Test
+    public void fibonacciReturnsInputValueWhenNegative() {
+        Assert.assertEquals(-1, Fibonacci.fibonacci(-1));
+        Assert.assertEquals(-2, Fibonacci.fibonacci(-2));
+        Assert.assertEquals(-3, Fibonacci.fibonacci(-3));
+        Assert.assertEquals(-4, Fibonacci.fibonacci(-4));
+        Assert.assertEquals(-5, Fibonacci.fibonacci(-5));
+    }
+
 }
