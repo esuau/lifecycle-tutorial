fb9b1149b7a0936f5a1662d2ec33de10c0b26e1b
esuau <evan.suau@etu.u-pec.fr>
2017-12-13 17:15:42 +0100

    TP-3 Implement LOG4J and store logs in a file

--- src/main/java/io/esuau/tutorial/Main.java
+++ src/main/java/io/esuau/tutorial/Main.java
@@ -1,8 +1,11 @@
 package io.esuau.tutorial;
 
 import java.util.Scanner;
+import org.apache.log4j.Logger;
 
 public class Main {
+
+    private final static Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -14,8 +17,12 @@ public class Main {
         } catch (NumberFormatException e) {
             n = 5;
         }
-        for (int i = 1; i <= n; i++)
-            System.out.println(i + ": " + fibonacci(i));
+        logger.info("n = " + n);
+        for (int i = 1; i <= n; i++) {
+            String str = i + ": " + fibonacci(i);
+            System.out.println(str);
+            logger.info(str);
+        }
     }
 
     private static long fibonacci(int n) {
